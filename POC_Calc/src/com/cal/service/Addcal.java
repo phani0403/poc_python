5b85a2b96ebb8d7771998a92f575e5e15dec17fa
phani.challa <phani.challa@HYDPCM218545L.ad.infosys.com>
2020-08-20 15:12:55 +0530

    Fisrt poc

---
+++ POC_Calc/src/com/cal/service/Addcal.java
@@ -0,0 +1,13 @@
+package com.cal.service;
+
+public class Addcal {
+	
+public double sum() {
+		
+	double res=30 + 10 + Calculator.PI ;
+		
+		return res;
+		
+	}
+
+}
---
+++ POC_Calc/src/com/cal/service/SubCal.java
@@ -0,0 +1,13 @@
+package com.cal.service;
+
+public class SubCal {
+	
+public double sub() {
+		
+	double res=30 - 10 - Calculator.PI ;
+		
+		return res;
+		
+	}
+
+}
